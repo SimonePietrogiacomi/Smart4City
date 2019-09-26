23f2b83d819c81c49758253e9cb9971fe6063764
Simone Pietrogiacomi <49372143+SimonePietrogiacomi@users.noreply.github.com>
2019-09-26 19:53:41 +0200

    Removed old Kafka Consumer

--- KafkaConsumer.py
+++
@@ -1,6 +0,0 @@
-from kafka import KafkaConsumer
-from json import loads
-
-consumer = KafkaConsumer('parking', value_deserializer=lambda x: loads(x.decode('utf-8')))
-for msg in consumer:
-    print(msg)
