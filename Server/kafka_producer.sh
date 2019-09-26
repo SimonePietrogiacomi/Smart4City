4441959eb03a4dac7a6b30d20f929055558e48f9
Simone Pietrogiacomi <49372143+SimonePietrogiacomi@users.noreply.github.com>
2019-09-26 21:45:53 +0200

    Update kafka_producer.sh

--- Server/kafka_producer.sh
+++ Server/kafka_producer.sh
@@ -5,5 +5,5 @@ kafka_path="~/big_data/kafka_2.11-2.3.0/bin/kafka-console-producer.sh"
 
 sed 1d $dataset_path/pollution/$1 | while read d
 do
-   echo $d | $kafka_path --broker-list localhost:9092 --topic new_topic
+   echo $d | $kafka_path --broker-list localhost:9092 --topic airPollution
 done
